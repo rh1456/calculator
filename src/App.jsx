23766904f3a7a28770b789d21fc9700f4528b6c0
Reid Hanson <reidhans@buffalo.edu>
2019-12-03 10:04:58 -0500

    forgot to push lastNight

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Button from './components/Button'
 
 const App = () => {
   return (
-    <div className="calculator-outline">
-      <div className="row">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>/ </button>
-      </div>
-      <div className="row">
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>* </button>
-      </div>
-      <div className="row">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+ </button>
-      </div>
-      <div className="row">
-        <button>.</button>
-        <button>0</button>
-        <button>=</button>
-        <button>-</button>
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Button} />
+      </Switch>
+    </Router>
   )
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Button = () => {
-  return <div className="app"></div>
+  const [operator, setOperator] = useState([])
+
+  const operatorUsed = val => {
+    setOperator(prev => {
+      return prev + val
+    })
+  }
+  const clearFunction = () => {
+    setOperator([])
+  }
+
+  const calculate = () => {
+    if (operator[1] === '-') {
+      setOperator(parseInt(operator[0]) - parseInt(operator[2]))
+    } else if (operator[1] === '+') {
+      setOperator(parseInt(operator[0]) + parseInt(operator[2]))
+    } else if (operator[1] === '*') {
+      setOperator(parseInt(operator[0]) * parseInt(operator[2]))
+    } else if (operator[1] === '/') {
+      setOperator(parseInt(operator[0]) / parseInt(operator[2]))
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="calculator-outline">
+        <div className="operator">{operator}</div>
+        <div className="row">
+          <button className="clear" onClick={() => clearFunction}>
+            C
+          </button>
+        </div>
+        <div className="row">
+          <button className="digits" onClick={() => operatorUsed(7)}>
+            7
+          </button>
+          <button className="digits" onClick={() => operatorUsed(8)}>
+            8
+          </button>
+          <button className="digits" onClick={() => operatorUsed(9)}>
+            9
+          </button>
+          <button className="mathFunction" onClick={() => operatorUsed('/')}>
+            /
+          </button>
+        </div>
+        <div className="row">
+          <button className="digits" onClick={() => operatorUsed(4)}>
+            4
+          </button>
+          <button className="digits" onClick={() => operatorUsed(5)}>
+            5
+          </button>
+          <button button className="digits" onClick={() => operatorUsed(6)}>
+            6
+          </button>
+          <button className="mathFunction" onClick={() => operatorUsed('*')}>
+            *
+          </button>
+        </div>
+        <div className="row">
+          <button className="digits" onClick={() => operatorUsed(1)}>
+            1
+          </button>
+          <button className="digits" onClick={() => operatorUsed(2)}>
+            2
+          </button>
+          <button className="digits" onClick={() => operatorUsed(3)}>
+            3
+          </button>
+          <button className="mathFunction" onClick={() => operatorUsed('+')}>
+            +
+          </button>
+        </div>
+        <div className="row">
+          <button className="digits" onClick={() => operatorUsed(0)}>
+            0
+          </button>
+          <button className="mathFunction" onClick={() => calculate()}>
+            =
+          </button>
+          <button className="mathFunction" onClick={() => operatorUsed('-')}>
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Button
